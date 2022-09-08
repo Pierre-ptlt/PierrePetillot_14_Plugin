90f1bcd691ff25080eba104b5c003b6ca0bec9c3
Pierre Petillot <petill_p@etna-alternance.net>
2022-09-08 18:03:17 +0200

    dropdown v1

---
+++ mydropdown/src/components/Dropdown.js
@@ -0,0 +1,19 @@
+import "./dropdown.css";
+
+function Dropdown(props) {
+	const options = props.data.map((item) => {
+		return (
+			<option key={item} value={item}>
+				{item}
+			</option>
+		);
+	});
+
+	return (
+		<select className="dropdown" name={props.name} id={props.id}>
+			{options}
+		</select>
+	);
+}
+
+export default Dropdown;
